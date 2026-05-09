ade86479c4b4302d114b70c53ae4d7322790a590
Rodion55921 <rodionkon77@gmail.com>
2026-05-09 10:12:56 +0300

    Update ooter_game.py
    размеры окна

--- ooter_game.py
+++ ooter_game.py
@@ -72,8 +72,8 @@ class Bullet(GameSprite):
             self.kill()
 
 
-win_width = 700
-win_height = 500
+win_width = 1300
+win_height = 700
 display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
